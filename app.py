43989cfb05e6f7e2d78532b488cbb516b17552fa
gaurav <gparajuli.1997@gmail.com>
2024-02-28 21:39:35 +0545

    endpoint created to add new store

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+stores = [
+    {
+        'name': 'My Store',
+        'items': [
+            {
+                'name': 'my item',
+                'price': 15.99
+            }
+        ]
+    }
+]
 
 @app.route('/')
 def index():
     return jsonify({
         'message': 'It works!'
     })
+
+@app.route('/store', methods=['GET', 'POST'])
+def store():
+    if request.method == 'GET':
+        return jsonify(stores=stores)
+    
+    # post request
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+
+    return new_store, 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
