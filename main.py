fb9b05cc3f848e51afa6ab679c2721b414d86a16
gaurav <gparajuli.1997@gmail.com>
2024-03-01 05:07:44 +0545

    added data validation checks

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import uuid
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.get('/')
 def index():
     return {
         'message': 'it works'
     }
 
-@app.route('/store/<string:store_id>')
+@app.get('/store/<string:store_id>')
 def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
+        # abort(404, message='store not found')
         return {'message': 'store not found'}, 404
     
 @app.get('/store')
@@ -26,6 +27,15 @@ def get_stores():
 def create_store():
 
     store_data = request.get_json()
+
+    if 'name' not in store_data:
+        return {'message': "'name' should be included in json payload"}, 400
+    
+    for store in stores.values():
+        if store['name'] == store_data['name']:
+            # abort(400, message=f'store already exits')
+            return {'message': 'store already exists'}, 400
+
     store_id = uuid.uuid4().hex
     store = {**store_data, 'id': store_id}
     stores[store_id] = store
@@ -36,7 +46,16 @@ def create_store():
 def create_item():
 
     item_data = request.get_json()
+
+    if (
+        'price' not in item_data
+        or 'store_id' not in item_data
+        or 'name' not in item_data
+    ):
+        return {'message': 'price, store_id and name should be included'}, 400
+
     if item_data['store_id'] not in stores:
+        # abort(404, message='store not found')
         return {'message': 'store not found'}, 404
     
     item_id = uuid.uuid4().hex
@@ -54,7 +73,8 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
+        # abort(404, message='item not found')
         return {'message': 'item not found'}, 404
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
