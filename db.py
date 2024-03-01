1060d0257b19acac4faaa22181ba43c0af876405
gaurav <gparajuli.1997@gmail.com>
2024-03-01 08:17:22 +0545

    created store and store_item model

--- db.py
+++ db.py
@@ -1,2 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
 stores = {}
 items = {}
+
+db = SQLAlchemy()
+
+class ItemModel(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    
