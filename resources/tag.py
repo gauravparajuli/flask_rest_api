31e63095b6896d30dd19d1a0971e0e61952f2a55
gaurav <gparajuli.1997@gmail.com>
2024-03-03 15:27:06 +0545

    added endpoints associating tags and items

--- resources/tag.py
+++ resources/tag.py
@@ -3,10 +3,46 @@ from flask_smorest import abort, Blueprint
 from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
-from models import TagModel, StoreModel
-from resources.schemas import TagSchema
+from models import TagModel, StoreModel, ItemModel
+from resources.schemas import TagSchema, TagAndItemSchema
 
 blp = Blueprint('Tags', __name__, description='Operations on tags')
+
+
+@blp.route('/item/<string:item_id>/tag/<string:tag_id>')
+class LinkTagToItem(MethodView):
+    @blp.response(201, TagSchema)
+    def post(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.append(tag)
+
+        if item.store.id != tag.store.id:
+            abort(400, message='make sure that item and tag belongs to the same store')
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, 'an error encountered while inserting the tag')
+
+        return tag
+    
+    @blp.response(204, TagAndItemSchema)
+    def delete(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.remove(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message='an error encountered while inserting the tag')
+
+        return dict(message='item removed from tag', item=item, tag=tag)
 
 @blp.route('/tag/<string:tag_id>')
 class Tag(MethodView):
@@ -14,6 +50,26 @@ class Tag(MethodView):
     def get(self, tag_id):
         tag = TagModel.query.get_or_404(tag_id)
         return tag
+    
+    @blp.response(
+        202, 
+        description='delete a tag if no item is associated with it',
+        example={'message': 'tag deleted'}
+    )
+    @blp.alt_response(404, description='tag not found')
+    @blp.alt_response(400, description='returned if the tag is assigned to one or more item. In this case, tag is not deleted')
+    def delete(self, tag_id):
+        tag = TagModel.query.get(tag_id)
+
+        if not tag:
+            abort(404, message='tag not found')
+
+        if not tag.items:
+            db.session.delete(tag)
+            db.session.commit()
+            return {}, 204
+        
+        abort(400, message='could not delete tag, tag is associated with items')
 
 @blp.route('/store/<string:store_id>/tag')
 class TagsInStore(MethodView):
